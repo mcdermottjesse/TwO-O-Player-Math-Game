468fa6dc1fa9e19ce3f9e29810aecee43fe2243c
Jesse McDermott <mcdermott.jesse9@gmail.com>
2021-03-29 18:50:29 +0000

    create players class

---
+++ player_class.rb
@@ -0,0 +1,19 @@
+class Player
+  attr_accessor :name, :lives
+
+  def initialize(name)
+    @name = name
+    @lives = 3
+  end
+
+  def lose_life
+    @lives -= 1
+  end
+end
+
+terry = Player.new("Terry")
+
+puts terry.name
+
+puts terry.lose_life
+
